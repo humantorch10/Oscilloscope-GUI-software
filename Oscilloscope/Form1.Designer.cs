04c74864be1199f6011886ba610bc663700f1934
humantorch10 <65015755+humantorch10@users.noreply.github.com>
2021-02-27 17:33:12 +0200

    Add project files.

---
+++ Oscilloscope/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Oscilloscope
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 
+        
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.BTN_CON = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CMB_BAUD = new System.Windows.Forms.ComboBox();
+            this.CMB_COM = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.CMB_DATA = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CMB_AVE = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // BTN_CON
+            // 
+            this.BTN_CON.Location = new System.Drawing.Point(273, 21);
+            this.BTN_CON.Name = "BTN_CON";
+            this.BTN_CON.Size = new System.Drawing.Size(118, 22);
+            this.BTN_CON.TabIndex = 1;
+            this.BTN_CON.Text = "CONNECT";
+            this.BTN_CON.UseVisualStyleBackColor = true;
+            this.BTN_CON.Click += new System.EventHandler(this.BTN_CON_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.CMB_BAUD);
+            this.groupBox1.Controls.Add(this.CMB_COM);
+            this.groupBox1.Controls.Add(this.BTN_CON);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 48);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COM Config";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baud Rate";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COM";
+            // 
+            // CMB_BAUD
+            // 
+            this.CMB_BAUD.FormattingEnabled = true;
+            this.CMB_BAUD.Location = new System.Drawing.Point(194, 20);
+            this.CMB_BAUD.Name = "CMB_BAUD";
+            this.CMB_BAUD.Size = new System.Drawing.Size(73, 21);
+            this.CMB_BAUD.TabIndex = 6;
+            // 
+            // CMB_COM
+            // 
+            this.CMB_COM.FormattingEnabled = true;
+            this.CMB_COM.Location = new System.Drawing.Point(45, 21);
+            this.CMB_COM.Name = "CMB_COM";
+            this.CMB_COM.Size = new System.Drawing.Size(78, 21);
+            this.CMB_COM.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.CMB_DATA);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.CMB_AVE);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(427, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(332, 48);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Graph Setting";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(277, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(51, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Save";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            // 
+            // CMB_DATA
+            // 
+            this.CMB_DATA.FormattingEnabled = true;
+            this.CMB_DATA.Location = new System.Drawing.Point(185, 20);
+            this.CMB_DATA.Name = "CMB_DATA";
+            this.CMB_DATA.Size = new System.Drawing.Size(86, 21);
+            this.CMB_DATA.TabIndex = 3;
+            this.CMB_DATA.SelectedIndexChanged += new System.EventHandler(this.CMB_DATA_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Data";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // CMB_AVE
+            // 
+            this.CMB_AVE.FormattingEnabled = true;
+            this.CMB_AVE.Location = new System.Drawing.Point(58, 20);
+            this.CMB_AVE.Name = "CMB_AVE";
+            this.CMB_AVE.Size = new System.Drawing.Size(86, 21);
+            this.CMB_AVE.TabIndex = 1;
+            this.CMB_AVE.SelectedIndexChanged += new System.EventHandler(this.CMB_AVE_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Average";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // chart2
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea3);
+            legend3.Enabled = false;
+            legend3.Name = "Legend1";
+            this.chart2.Legends.Add(legend3);
+            this.chart2.Location = new System.Drawing.Point(12, 102);
+            this.chart2.Name = "chart2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.IsVisibleInLegend = false;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart2.Series.Add(series3);
+            this.chart2.Size = new System.Drawing.Size(738, 273);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(612, 76);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(86, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(451, 77);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(285, 77);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(118, 20);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(462, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Amplitude Scale";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(306, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Time Scale";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 413);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "InterfaceTask";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button BTN_CON;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox CMB_COM;
+        private System.Windows.Forms.ComboBox CMB_BAUD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CMB_AVE;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CMB_DATA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Oscilloscope/Form1.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Text.RegularExpressions;
+
+
+namespace Oscilloscope
+{
+	public partial class Form1 : Form
+	{
+
+		int MAX_HISTORY = 30;
+		int AVERAGE = 1;
+		int check_channel = 1;
+		private readonly object Lock1 = new object();
+		private readonly object Lock2 = new object();
+		Queue<double> Qdata2 = new Queue<double>();
+		Queue<double> Qdata3 = new Queue<double>();
+		int count;
+		double sum2;
+		private int timeScale = 0;
+		private double ampScale = 0;
+
+
+
+		private class BuadItem : Object
+		{
+			private string name = "";
+			private int val = 0;
+			
+			public string NAME
+			{
+				set { name = value; }
+				get { return name; }
+			}    
+		
+			public int BAUDRATE
+			{
+				set { val = value; }
+				get { return val; }
+			}
+		   
+			public override string ToString()
+			{
+				return name;
+			}
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void BTN_CON_Click(object sender, EventArgs e)
+		{
+			if (serialPort1.IsOpen == true)
+			{
+
+				try
+				{
+					System.Windows.Forms.Application.DoEvents();
+					if (true == serialPort1.IsOpen) serialPort1.DiscardInBuffer();
+					System.Windows.Forms.Application.DoEvents();
+					if (true == serialPort1.IsOpen) serialPort1.Close();
+					CMB_BAUD.Enabled = true;
+					CMB_COM.Enabled = true;
+					System.Windows.Forms.Application.DoEvents();
+					count = 1;
+
+					BeginInvoke(new Delegate_ChangeButton(ChangeButton), new Object[] { "Connect" });
+
+				}
+				catch (Exception ex){ Console.WriteLine(ex); }
+				
+			}
+			else {
+
+				serialPort1.PortName = CMB_COM.SelectedItem.ToString();
+				
+				BuadItem baud = (BuadItem)CMB_BAUD.SelectedItem;
+				serialPort1.BaudRate = baud.BAUDRATE;				
+				serialPort1.DataBits = 8;				
+				serialPort1.Parity = Parity.None;				
+				serialPort1.StopBits = StopBits.One;	
+				serialPort1.Handshake = Handshake.None;
+				serialPort1.Encoding = Encoding.ASCII;
+				CMB_BAUD.Enabled = false;
+				CMB_COM.Enabled = false;
+				count = 1;
+
+						
+				try
+				{
+					serialPort1.Open();
+					if (true == serialPort1.IsOpen) serialPort1.DiscardInBuffer();
+					BeginInvoke(new Delegate_ChangeButton(ChangeButton), new Object[] { "Disconnect" });
+				   
+				}
+				catch (Exception ex){ Console.WriteLine(ex); }
+			}
+
+		}
+
+		private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+		{
+
+			BeginInvoke(new Delegate_GetDataText(GetDataText), new Object[] { CMB_DATA });
+			BeginInvoke(new Delegate_GetAveText(GetAveText), new Object[] { CMB_AVE });
+			BeginInvoke(new Delegate_GetTimeScale(GetTimeScale), new Object[] { numericUpDown2 });
+			BeginInvoke(new Delegate_GetAmpScale(GetAmpScale), new Object[] { numericUpDown1 });
+
+
+			string[] StrArryData = new string[10];
+			string rcvData="";
+
+
+			if( serialPort1.IsOpen == false )	return;
+
+			check_channel = 1 - check_channel; // identify channel
+			try {
+				string comdata =  serialPort1.ReadLine();//ReadExisting()
+				rcvData = comdata;
+				StrArryData = comdata.Split(new string[] { "@",";" }, StringSplitOptions.RemoveEmptyEntries);
+				//for (int i = 1; i < StrArryData.Length; i++)
+				//{
+				//	
+				//}
+				StrArryData[0] = StrArryData[0].Substring(0, 2);
+			}
+			catch (Exception ex){ Console.WriteLine(ex);}
+
+
+
+
+			double tmp1 = 0.0;
+			 
+			if (StrArryData.Length >= 1) { 
+				double.TryParse( Regex.Replace(StrArryData[0], "[^.0-9-]", ""), out tmp1);
+			}
+
+			
+			
+			if (count < AVERAGE)
+			{
+				sum2 = sum2 + tmp1;
+				count++;
+			}
+			else {
+				
+				sum2 = sum2 + tmp1;
+				if (check_channel == 0)
+				{
+					Qdata2.Enqueue(sum2 / (double)AVERAGE);
+					sum2 = 0;
+					count = 1;
+				}
+				else if (check_channel == 1)
+				{
+					Qdata3.Enqueue(sum2 / (double)AVERAGE);
+					sum2 = 0;
+					count = 1;
+				}
+
+			}
+
+		   
+			while (Qdata2.Count > MAX_HISTORY) Qdata2.Dequeue();
+
+			while (Qdata3.Count > MAX_HISTORY) Qdata3.Dequeue();
+			
+
+			BeginInvoke(new Delegate_ShowChart(ShowChart2), new Object[] { chart2 });
+			
+			/* Testing values */
+			//BeginInvoke(new Delegate_ChangeTxtBox(ChangeTxtBox), new Object[] { rcvData });
+
+
+
+		}
+
+
+		private delegate void Delegate_ShowChart(Chart chart);
+		private delegate void Delegate_GetDataText(Control CON);
+		private delegate void Delegate_GetAveText(Control CON);
+		private delegate void Delegate_GetTimeScale(Control CON);
+		private delegate void Delegate_GetAmpScale(Control CON);
+		private delegate void Delegate_ChangeButton(string text);
+		private delegate void Delegate_ChangeTxtBox(string text);
+
+
+		private void ShowChart2(Chart chart)
+		{
+			double i = 0.0;
+			var Qdata2ar = Qdata2.ToArray();
+			var Qdata3ar = Qdata3.ToArray();
+			if ((String)comboBox1.SelectedItem == "Channel 1")
+			{
+				chart.Series[0].Points.Clear();
+				chart.Series[0].MarkerStyle = MarkerStyle.Circle;
+				chart.Series[0].MarkerSize = 6;
+				chart.Series[0].Color = Color.CornflowerBlue;
+				if (Qdata2.Count > 2)
+				{
+					chart.ChartAreas[0].AxisY.ScaleView.Size = ampScale;
+					chart.ChartAreas[0].AxisX.ScaleView.Size = timeScale;
+
+				}
+
+					//foreach (int value in Qdata2)
+					//{
+					//	chart.Series[0].Points.Add(new DataPoint(i, value));
+					//	i += 0.20;
+					//}
+
+					for (int j = 0; j < Qdata2ar.Length; j++)
+					{
+						chart.Series[0].Points.Add(new DataPoint(i, Qdata2ar[j]));
+						i += 0.5;
+					}
+
+				}
+			if ((String)comboBox1.SelectedItem == "Channel 2")
+			{
+				chart.Series[0].Points.Clear();
+				chart.Series[0].MarkerStyle = MarkerStyle.Square;
+				chart.Series[0].MarkerSize = 6;
+				chart.Series[0].Color = Color.DarkRed;
+				if (Qdata3.Count > 2)
+				{
+					chart.ChartAreas[0].AxisY.ScaleView.Size = ampScale;
+					chart.ChartAreas[0].AxisX.ScaleView.Size = timeScale;
+
+				}
+
+					//foreach (int value in Qdata3)
+					//{
+					//	chart.Series[0].Points.Add(new DataPoint(i, value));
+					//	i += 0.20;
+					//}
+
+					for (int j = 0; j < Qdata3ar.Length; j++)
+					{
+						chart.Series[0].Points.Add(new DataPoint(i, Qdata3ar[j]));
+						i += 0.5;
+					}
+
+
+				}
+		}
+
+
+
+
+		private void GetAmpScale(Control CON)
+		{
+			double.TryParse(CON.Text, out ampScale);
+			
+		}
+		private void GetTimeScale(Control CON)
+		{
+			int.TryParse(CON.Text, out timeScale);
+			
+		}
+		private void GetDataText(Control CON)
+		{
+			int.TryParse(CON.Text, out MAX_HISTORY);
+			
+		}
+
+		private void GetAveText(Control CON)
+		{
+
+			int.TryParse(CON.Text, out AVERAGE);
+
+		}
+
+		private void ChangeButton(string text) {
+
+			BTN_CON.Text = text;
+		
+		}
+
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Init_Control();
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			try
+			{
+				if (true == serialPort1.IsOpen) serialPort1.DiscardInBuffer();
+				System.Windows.Forms.Application.DoEvents();
+				if (serialPort1.IsOpen) serialPort1.Close();
+				System.Windows.Forms.Application.DoEvents();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+		}
+
+
+		private void Init_Control(){
+
+			/*Set COM Port List*/
+			string[] ports = SerialPort.GetPortNames();
+			CMB_COM.Items.Clear();
+			foreach (string prtname in ports)
+			{
+				CMB_COM.Items.Add(prtname);
+			}
+			if (CMB_COM.Items.Count > 0)
+			{
+				CMB_COM.SelectedIndex = CMB_COM.Items.Count - 1;
+			}
+
+			/*Set Baud Rate*/
+			BuadItem baud;
+			baud = new BuadItem();
+			baud.NAME = "4800bps";
+			baud.BAUDRATE = 4800;
+			CMB_BAUD.Items.Add(baud);
+
+			baud = new BuadItem();
+			baud.NAME = "9600bps";
+			baud.BAUDRATE = 9600;
+			CMB_BAUD.Items.Add(baud);
+
+			baud = new BuadItem();
+			baud.NAME = "19200bps";
+			baud.BAUDRATE = 19200;
+			CMB_BAUD.Items.Add(baud);
+
+			baud = new BuadItem();
+			baud.NAME = "115200bps";
+			baud.BAUDRATE = 115200;
+			CMB_BAUD.Items.Add(baud);
+			CMB_BAUD.SelectedIndex = 1;
+
+			/* set channels */
+			comboBox1.Items.Add("Channel 1");
+			comboBox1.Items.Add("Channel 2");
+
+			/*Set Average List*/
+			CMB_AVE.Items.Add(1);
+			for (int i = 1; i < 11; i++)
+			{
+				CMB_AVE.Items.Add(2 * i);
+			}
+			CMB_AVE.SelectedIndex = 0;
+
+			/*Set Data List*/
+			CMB_DATA.Items.Add(1);
+			for (int i = 1; i < 11; i++)
+			{
+				CMB_DATA.Items.Add(i * 30);
+			}
+			CMB_DATA.SelectedIndex = 3;
+		}
+
+		public static int CountChar(string s, char c)
+		{
+			return s.Length - s.Replace(c.ToString(), "").Length;
+		}
+
+		private void CMB_DATA_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void CMB_AVE_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void groupBox2_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void chart2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label4_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label5_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
