1a450fdc422968a050ad071558ec67ae9be6b427
humantorch10 <65015755+humantorch10@users.noreply.github.com>
2021-03-01 01:37:16 +0200

    Additional files to the project
    C embedded code

---
+++ Code-Final/Code/Task5/main.c
@@ -0,0 +1,16 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "uart.h"
+#include "oscilloscope.h"
+#include <avr/io.h>
+
+
+int main(void)
+{
+	OSCI_Init();
+	sei();
+	while (1)
+	{
+		OSCI_MainFunction();
+	}
+}
